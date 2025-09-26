5c6691953d8a683f4e1c0af4ec53c768964f8793
DebDev25 <bdesperado24@gmail.com>
2025-09-27 00:52:16 +0530

    Basic Backend commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import datetime
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template(
+        "index.html",
+        year=datetime.date.today().year,
+    )
+
+@app.route("/patient")
+def patient():
+    return render_template(
+        "patient.html"
+    )
+
+@app.route("/caretaker")
+def caretaker():
+    return render_template(
+        "caretaker.html"
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
