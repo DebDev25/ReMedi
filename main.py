d087e447fb43d213c3e107e55d0281ca4a7d19a3
DebDev25 <bdesperado24@gmail.com>
2025-09-27 09:30:53 +0530

    added variable tracking for better backend integration

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import datetime
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
@@ -10,17 +12,20 @@ def home():
         year=datetime.date.today().year,
     )
 
+
 @app.route("/patient")
 def patient():
     return render_template(
         "patient.html"
     )
 
+
 @app.route("/caretaker")
 def caretaker():
     return render_template(
         "caretaker.html"
     )
+
 
 if __name__ == "__main__":
     app.run(debug=True)
